f077ad9657333173b178f4f701b94484b71c0e34
Sanyam2511 <sanyamphartyal822@gmail.com>
2026-05-27 16:16:40 +0530

    fix worker cors

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -54,7 +54,7 @@ app.use((req, res, next) => {
             res.setHeader('Vary', 'Origin');
         }
         res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         res.setHeader('Access-Control-Max-Age', '86400');
     }
 
